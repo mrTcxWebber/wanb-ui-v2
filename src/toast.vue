a07e97dbf0357d958dd26ccd58af053b5e76a40a
mrt <814463173@qq.com>
2020-09-30 07:53:45 +0800

    add toast

---
+++ src/toast.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="v-toast">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'wToast',
+
+    }
+</script>
+
+<style lang="" scoped>
+    .v-toast {}
+</style>
